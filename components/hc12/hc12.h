8247a8732f88ef3df0beddcf15f132d792a59096
Maduka Jayalath <madu.rapa@gmail.com>
2025-02-17 07:43:56 +0530

    code fomating

--- components/hc12/hc12.h
+++ components/hc12/hc12.h
@@ -4,37 +4,42 @@
 #include "esphome/components/uart/uart.h"
 #include <memory>
 
-namespace esphome {
-namespace hc12 {
+namespace esphome
+{
+  namespace hc12
+  {
 
-class HC12Component : public Component, public uart::UARTDevice {
- public:
-  explicit HC12Component(uart::UARTComponent* parent) : uart::UARTDevice(parent) {}
-  
-  void setup() override;
-  void loop() override;
-  void dump_config() override;
-  
-  void send_message(const std::string& message);
-  void set_callback(std::function<void(const std::string&)> callback) { 
-    callback_ = std::move(callback); 
-  }
-  
-  float get_setup_priority() const override { 
-    return setup_priority::LATE; 
-  }
+    class HC12Component : public Component, public uart::UARTDevice
+    {
+    public:
+      explicit HC12Component(uart::UARTComponent *parent) : uart::UARTDevice(parent) {}
 
- protected:
-  bool is_valid_char(char c) const;
-  void process_buffer();
-  std::string sanitize_message(const std::string& message) const;
+      void setup() override;
+      void loop() override;
+      void dump_config() override;
 
- private:
-  std::string buffer_;
-  std::function<void(const std::string&)> callback_{nullptr};
-  static constexpr char MESSAGE_TERMINATOR = '\n';
-  static constexpr size_t MAX_BUFFER_SIZE = 256;
-};
+      void send_message(const std::string &message);
+      void set_callback(std::function<void(const std::string &)> callback)
+      {
+        callback_ = std::move(callback);
+      }
 
-}  // namespace hc12
-}  // namespace esphome
+      float get_setup_priority() const override
+      {
+        return setup_priority::LATE;
+      }
+
+    protected:
+      bool is_valid_char(char c) const;
+      void process_buffer();
+      std::string sanitize_message(const std::string &message) const;
+
+    private:
+      std::string buffer_;
+      std::function<void(const std::string &)> callback_{nullptr};
+      static constexpr char MESSAGE_TERMINATOR = '\n';
+      static constexpr size_t MAX_BUFFER_SIZE = 256;
+    };
+
+  } // namespace hc12
+} // namespace esphome
